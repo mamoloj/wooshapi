2d13948460ac8ed0d2df0a571e07f86012d33601
unknown <mamoloj@gmail.com>
2022-08-28 03:04:09 +0800

    fixing issue on db

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,12 +3,9 @@ import token
 from fastapi import APIRouter , Depends , status , HTTPException , Response
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from ..database import get_db
-from .. import schemas , models , utils , oauth2
+from .. import schemas , models , utils ,  main , oauth2
 from sqlalchemy.orm import Session
 import psycopg2
-from psycopg2.extras import RealDictCursor
-conn = psycopg2.connect(host="localhost",database="production",user="postgres",password="password",cursor_factory=RealDictCursor)
-cursor = conn.cursor()
 
 
 router = APIRouter(tags=['Authentication'])
@@ -16,16 +13,13 @@ router = APIRouter(tags=['Authentication'])
 @router.post("/login")
 def login(user_creds: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     #for OAuth2PasswordRequestForm form always use username instead email = user_creds.username
-    print(user_creds.username)
     #user = db.query(models.AccountsUser).filter(models.AccountsUser.email == user_creds.username).first()
-    cursor.execute("select id , name , password from accounts_user where email = %s LIMIT 1",(str(user_creds.username),))
-    user = cursor.fetchone()
-    conn.commit()
+    main.cursor.execute("select id , name , password from accounts_user where email = %s LIMIT 1",(str(user_creds.username),))
+    user = main.cursor.fetchone()
+    main.conn.commit()
     if not user :
-        print("no user")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credetials")
     if not utils.verify_pass(user_creds.password,"$2b$12$yNEVfzM2e7H51FF.4UN/D.QV0lRry/WuvCjLERtFKAdBey3t9/Zzi"):
-        print("invalid cred")
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail=f"Invalid Credetials")
 
     access_token = oauth2.create_access_token(data = { "user_id" : user['id'] })
